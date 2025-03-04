45aa3be3bc8ffef9c9db3081d63dfd851428f049
Andresito126 <creed126ax@gmail.com>
2025-03-04 14:30:34 -0600

    Add:Some smal changes in develop

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	
-
+	coursesBD "github.com/Andresito126/go-estudiantes/src/courses/infrastructure/dependencies"
+	coursesRoutes "github.com/Andresito126/go-estudiantes/src/courses/infrastructure/routes"
 	studentsBD "github.com/Andresito126/go-estudiantes/src/students/infrastructure/dependencies"
 	studentsRoutes "github.com/Andresito126/go-estudiantes/src/students/infrastructure/routes"
-	coursesRoutes "github.com/Andresito126/go-estudiantes/src/courses/infrastructure/routes"
-	coursesBD "github.com/Andresito126/go-estudiantes/src/courses/infrastructure/dependencies"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +13,7 @@ func main() {
     studentsBD.Init()
 	coursesBD.Init()
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	studentsRoutes.StudentRoutes(r)	
 	coursesRoutes.CoursesRoutes(r)
