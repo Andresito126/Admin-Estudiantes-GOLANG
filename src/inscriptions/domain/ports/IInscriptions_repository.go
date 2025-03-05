c23ed1dcc33086036a36d75c0cd3b7442e5ee3c0
Andresito126 <creed126ax@gmail.com>
2025-03-05 00:54:34 -0600

    Add:domain done

---
+++ src/inscriptions/domain/ports/IInscriptions_repository.go
@@ -0,0 +1,11 @@
+package ports
+
+import "github.com/Andresito126/go-estudiantes/src/inscriptions/domain/entities"
+
+type IInscriptionsRepository interface {
+
+	Save(inscription *entities.Inscription) error
+	UpdateStatus(id int, status entities.InscriptionStatus) error
+	FindByID(id int) (*entities.Inscription, error)
+	
+}
